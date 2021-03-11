b949d451d106ebaf66a836241c3037a78d24983b
Lukas <tinnes-lukas@gmx.de>
2021-03-11 19:58:23 +0100

    added random text on load

--- app/src/main/java/com/example/depressionquotes/MainActivity.java
+++ app/src/main/java/com/example/depressionquotes/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.depressionquotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        This is good coding practice for apps apparently
+        String[] quotes = getResources().getStringArray(R.array.quotes);
+        final TextView textview = findViewById(R.id.text_id);
+        textview.setText(quotes[0]);
     }
 }
